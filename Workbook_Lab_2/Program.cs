f811859a401cd9ec4b98c1c48217f175956afbfa
Alejandra Roel <alejandraroel@Alejandras-MacBook-Air-2.local>
2020-01-22 17:01:51 -0500

    Final Edit

--- Workbook_Lab_2/Program.cs
+++ Workbook_Lab_2/Program.cs
@@ -9,7 +9,9 @@ namespace Workbook_Lab_2
         {
 
             decimal length, width;
+            decimal height;
             string yesOrNo;
+
 
             Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
 
@@ -19,44 +21,96 @@ namespace Workbook_Lab_2
             Console.WriteLine("Enter length.");
             length = decimal.Parse(Console.ReadLine());
 
+            Console.WriteLine("Enter height.");
+            height = decimal.Parse(Console.ReadLine());
+
             decimal result = (width * length);
 
             decimal perimeter = 2 * length + 2 * width;
 
+            decimal volume = perimeter * height;
+
             Console.WriteLine("The area of the room is " + result);
             Console.WriteLine("The perimeter of the room is " + perimeter);
+            Console.WriteLine("The volume of the room is " + volume);
 
             Console.WriteLine("Continue? Y/N");
 
             string v = yesOrNo = Console.ReadLine();
 
 
-            if (v == "Y")
+            // ////////////////////////
+            // ///////////////////////
+
+            while
+
+                 (v == "Y" || v == "y")
+
+
             {
+                Console.WriteLine("Enter width.");
+                width = (decimal)Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter length.");
+                length = (decimal)Int32.Parse(Console.ReadLine());
+
+                result = width * length;
+
+                perimeter = 2 * length + 2 * width;
+
+                Console.WriteLine("The area of the room is " + result);
+
+                Console.WriteLine("The perimeter of the room is " + perimeter);
+
+                Console.WriteLine("Continue? Y/N");
+                v = Console.ReadLine();
+            }
 
 
-                {
-                    Console.WriteLine("Enter width.");
-                    width = (decimal)Int32.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Enter length.");
-                    length = (decimal)Int32.Parse(Console.ReadLine());
+            while (v == "N" || v == "n")
 
-                    result = width * length;
-
-                    perimeter = 2 * length + 2 * width;
-
-                    Console.WriteLine("The area of the room is " + result);
-
-                    Console.WriteLine("The perimeter of the room is " + perimeter);
-
-                    Console.WriteLine("Continue? Y/N");
-                    v = Console.ReadLine();
-                }
-
+            {
+                Console.WriteLine("Thank you for playing.");
+                break;
 
 
             }
+
+
+
+            while (v != "N" || v != "n" || v != "Y" || v != "y")
+
+            {
+
+                Console.WriteLine("Invalid Entry. Please try again.");
+                break;
+            }
+
+          
+
+                Console.WriteLine("Enter length.");
+                length = (decimal)Int32.Parse(Console.ReadLine());
+
+                result = width * length;
+
+                perimeter = 2 * length + 2 * width;
+
+                Console.WriteLine("The area of the room is " + result);
+
+                Console.WriteLine("The perimeter of the room is " + perimeter);
+
+                Console.WriteLine("Continue? Y/N");
+                v = Console.ReadLine();
+
+                Console.WriteLine("The perimeter of the room is " + perimeter);
+
+                Console.WriteLine("Continue? Y/N");
+                v = Console.ReadLine();
+
+
         }
+            
+            
     }
 }
